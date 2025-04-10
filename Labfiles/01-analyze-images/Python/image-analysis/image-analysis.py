75b9ee33f05a700b89f7f740b04b402bd21d1a05
NilsEngholm <Nils.engholm@gmail.com>
2025-04-10 09:31:58 -0600

    .

--- Labfiles/01-analyze-images/Python/image-analysis/image-analysis.py
+++ Labfiles/01-analyze-images/Python/image-analysis/image-analysis.py
@@ -7,7 +7,9 @@ from azure.core.exceptions import HttpResponseError
 import requests
 
 # Import namespaces
-
+from azure.ai.vision.imageanalysis import ImageAnalysisClient
+from azure.ai.vision.imageanalysis.models import VisualFeatures
+from azure.core.credentials import AzureKeyCredential
 
 def main():
     global cv_client
@@ -19,7 +21,7 @@ def main():
         ai_key = os.getenv('AI_SERVICE_KEY')
 
         # Get image
-        image_file = 'images/street.jpg'
+        image_file = 'Labfiles\\01-analyze-images\Python\image-analysis\images\image.jpg'
         if len(sys.argv) > 1:
             image_file = sys.argv[1]
 
@@ -27,7 +29,10 @@ def main():
             image_data = f.read()
 
         # Authenticate Azure AI Vision client
-
+        cv_client = ImageAnalysisClient(
+            endpoint=ai_endpoint,
+            credential=AzureKeyCredential(ai_key)
+            )
         
         # Analyze image
         AnalyzeImage(image_file, image_data, cv_client)
@@ -41,7 +46,15 @@ def AnalyzeImage(image_filename, image_data, cv_client):
 
     try:
         # Get result with specified features to be retrieved
-        
+        result = cv_client.analyze(
+            image_data=image_data,
+                visual_features=[
+                    VisualFeatures.CAPTION,
+                    VisualFeatures.DENSE_CAPTIONS,
+                    VisualFeatures.TAGS,
+                    VisualFeatures.OBJECTS,
+                    VisualFeatures.PEOPLE],
+                )
 
     except HttpResponseError as e:
         print(f"Status code: {e.status_code}")
@@ -49,6 +62,21 @@ def AnalyzeImage(image_filename, image_data, cv_client):
         print(f"Message: {e.error.message}")
 
     # Display analysis results
+    if result.caption is not None:
+        print("\nCaption:")
+        print(" Caption: '{}' (confidence: {:.2f}%)".format(result.caption.text, result.caption.confidence * 100))
+
+    # Get image dense captions
+    if result.dense_captions is not None:
+        print("\nDense Captions:")
+        for caption in result.dense_captions.list:
+            print(" Caption: '{}' (confidence: {:.2f}%)".format(caption.text, caption.confidence * 100))
+    
+    if result.tags is not None:
+        print("\nTags:")
+        for tag in result.tags.list:
+            print(" Tag: '{}' (confidence: {:.2f}%)".format(tag.name, tag.confidence * 100))
+
     
 
 
